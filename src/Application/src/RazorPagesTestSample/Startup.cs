1578b87144c5b589204132381b853f0bad1af05d
Deependra Chaudhary <41119735+DeepGit01@users.noreply.github.com>
2024-09-11 18:58:50 +0530

    Update Startup.cs

--- src/Application/src/RazorPagesTestSample/Startup.cs
+++ src/Application/src/RazorPagesTestSample/Startup.cs
@@ -19,7 +19,7 @@ namespace RazorPagesTestSample
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //added another comment to trigger the workflow and push the docker image -- comment updated
+            //added another comment to trigger the workflow and push the docker image -- comment updated again
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
